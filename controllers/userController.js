ab478ba7df2c1274e5bc7d9bfc6cee2f851b510b
technoprogramming92 <technoprogramming92@gmail.com>
2023-01-05 18:56:49 +0530

    Completed Middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -120,8 +120,24 @@ const logout = asyncHandler (async (req, res) => {
     return res.status(200).json({message: "Successfully Logged Out"});
 });
 
+//Get User Data
+const getUser = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if(user) {
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(200).json({
+            _id, name, email, photo, phone, bio,
+        });
+    }
+    else {
+        res.status(400);
+        throw new Error("User Not Found");
+    }
+});
+
 module.exports = {
     registerUser,
     loginUser,
     logout,
+    getUser,
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { registerUser, loginUser, logout } = require("../controllers/userController");
+const { registerUser, loginUser, logout, getUser } = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
+router.get("/getuser", protect, getUser);
 
 module.exports = router;
